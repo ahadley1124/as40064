89d6e24b9313076c68369c4983dae8f5973bae88
ahadley1124 <admin@austinh.dev>
2025-09-04 14:42:12 -0400

    Update About page
    Signed-off-by: ahadley1124 <admin@austinh.dev>

--- src/site/about.rs
+++ src/site/about.rs
@@ -11,8 +11,8 @@ pub fn about() -> Html {
             <p>{ "Amateur IX is an Internet Exchange designed for the AMPR network, supporting 44/9 and 44.128/10 address space. Our mission is to facilitate peering and connectivity for amateur radio IP networks, fostering collaboration and technical advancement in the community." }</p>
             <h2>{ "Exchange Policy" }</h2>
             <ul>
-                <li>{ "Open peering: All networks with AMPR address space (44/9, 44.128/10) are welcome to peer." }</li>
-                <li>{ "No commercial traffic: The exchange is strictly for amateur radio and research purposes." }</li>
+                <li>{ "Open peering: All networks with AMPR address space (44/9, 44.128/10) are welcome to peer. Any other networks will be evaluated before peering." }</li>
+                <li>{ "No commercial traffic: The exchange is strictly for amateur radio and research purposes. Transit providers will be allowed on a case by case basis and subject to voting by the members." }</li>
                 <li>{ "Respect and collaboration: Members are expected to maintain a respectful and collaborative environment." }</li>
                 <li>{ "Technical requirements: Participants must maintain secure and reliable connections, and follow best practices for routing and filtering." }</li>
                 <li>{ "Policy updates: The exchange policy may be updated as the community grows and needs evolve." }</li>
